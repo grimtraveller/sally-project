b7efe09875d6756aa381acbc60392e4d4240393e
derknob <derknob@9afc4399-3f22-6faa-62dd-eea3a5e6d859>
2010-12-03 13:58:16 +0000

    updated

--- trunk/___scripts/createApiInstaller.ps1
+++ trunk/___scripts/createApiInstaller.ps1
@@ -3,7 +3,7 @@
 # empty include folder
 remove-item $startDir"sally\__apiInstall\include\include\sallyAPI\*"
 
-copy-item $startDir"sally\sallyAPI\*.h" $startDir"sally\__apiInstall\include\include\sallyAPI\"
+copy-item $startDir"sally\sally\sallyAPI\*.h" $startDir"sally\__apiInstall\include\include\sallyAPI\"
 
-copy-item $startDir"sally\Release\sallyAPI.lib" $startDir"sally\__apiInstall\lib\lib\"
-copy-item $startDir"sally\Release\sallyAPI.pdb" $startDir"sally\__apiInstall\lib\lib\"
+copy-item $startDir"sally\sally\Release\sallyAPI.lib" $startDir"sally\__apiInstall\lib\lib\"
+copy-item $startDir"sally\sally\Release\sallyAPI.pdb" $startDir"sally\__apiInstall\lib\lib\"
