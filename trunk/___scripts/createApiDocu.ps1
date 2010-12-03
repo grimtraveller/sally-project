d4abdfcb15e27232821cb6cb7f90e87316ddb455
derknob <derknob@9afc4399-3f22-6faa-62dd-eea3a5e6d859>
2010-12-03 13:59:26 +0000

    renamed createApiDoc.ps1

--- trunk/___scripts/createApiDocu.ps1
+++
@@ -1,9 +0,0 @@
-. ".\_setEnv.ps1"
-
-# empty include folder
-remove-item $startDir"sally\__apiInstall\include\include\sallyAPI\*"
-
-copy-item $startDir"sally\sallyAPI\*.h" $startDir"sally\__apiInstall\include\include\sallyAPI\"
-
-copy-item $startDir"sally\Release\sallyAPI.lib" $startDir"sally\__apiInstall\lib\lib\"
-copy-item $startDir"sally\Release\sallyAPI.pdb" $startDir"sally\__apiInstall\lib\lib\"
