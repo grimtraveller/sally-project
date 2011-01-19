a02f0b847759d27af2989ed95a35bb2b4df9e433
derknob <derknob@9afc4399-3f22-6faa-62dd-eea3a5e6d859>
2011-01-19 12:45:41 +0000

    added -ErrorAction SilentlyContinue if the Release folder does not exist

--- ___scripts/cleanProjects.ps1
+++ ___scripts/cleanProjects.ps1
@@ -12,7 +12,7 @@ cmd /c start /wait $devenv $startDir"sallyPlugins\sally Plugins.sln" /clean Rele
 cmd /c start /wait $devenv $startDir"scummvm\release-1-1-1\dists\msvc9\scummvm.sln" /project sally /projectconfig Debug /clean Debug
 cmd /c start /wait $devenv $startDir"scummvm\release-1-1-1\dists\msvc9\scummvm.sln" /project sally /projectconfig Release /clean Release
 
-get-childitem $startDir"sallyPlugins\Release\" -include *.pdb -recurse | foreach ($_) { remove-item $_.fullname -verbose -ErrorAction SilentlyContinue }
-get-childitem $startDir"sallyPlugins\Release\" -include *.dll -recurse | foreach ($_) { remove-item $_.fullname -verbose -ErrorAction SilentlyContinue }
+get-childitem $startDir"sallyPlugins\Release\" -include *.pdb -recurse -ErrorAction SilentlyContinue | foreach ($_) { remove-item $_.fullname -verbose -ErrorAction SilentlyContinue }
+get-childitem $startDir"sallyPlugins\Release\" -include *.dll -recurse -ErrorAction SilentlyContinue | foreach ($_) { remove-item $_.fullname -verbose -ErrorAction SilentlyContinue }
 
 echo "+++ cleanProjects - DONE +++"
