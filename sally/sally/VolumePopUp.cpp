c0da9c71a6e87596516f1d925a1b091cd1f82447
derknob <derknob@9afc4399-3f22-6faa-62dd-eea3a5e6d859>
2011-03-28 13:55:59 +0000

    [SA-00407] Sally: auto hide VolumeControl after 15 sec

--- sally/sally/VolumePopUp.cpp
+++ sally/sally/VolumePopUp.cpp
@@ -28,24 +28,19 @@
 #include "VolumePopUp.h"
 
 #define GUI_MUTED	50001
+#define HIDE_TIMER	8000
 
 CVolumePopUp::CVolumePopUp(SallyAPI::GUI::CGUIBaseObject* parent)
 	:SallyAPI::GUI::CPopUpWindow(parent, 0, "")
 {
+	SetCloseOnClick(true);
+
 	m_pBackground = new SallyAPI::GUI::CGroupBox(this, (WINDOW_WIDTH - 644) / 2, (WINDOW_HEIGHT - 200), 644, 102);
 	this->AddChild(m_pBackground);
 
 	m_pVolumeControl = new SallyAPI::GUI::CVolumeControl(this, (WINDOW_WIDTH - 644) / 2 + 20, (WINDOW_HEIGHT - 200) + 20,
 		644 - 40);
 	this->AddChild(m_pVolumeControl);
-
-// 	m_pVolumeSlider = new SallyAPI::GUI::CSlider(this, (WINDOW_WIDTH - 644) / 2 + 30 + 40, (WINDOW_HEIGHT - 200) + 20, 544);
-// 	m_pVolumeSlider->SetMaxPosition(1000);
-// 	this->AddChild(m_pVolumeSlider);
-// 
-// 	m_pVolumeMute = new SallyAPI::GUI::CButton(this, (WINDOW_WIDTH - 644) / 2 + 20, (WINDOW_HEIGHT - 200) + 20, CONTROL_HEIGHT, CONTROL_HEIGHT, GUI_MUTED);
-// 	m_pVolumeMute->SetImageId(GUI_THEME_SALLY_AUDIO_MUTED);
-// 	this->AddChild(m_pVolumeMute);
 
 	m_pVolumeLow = new SallyAPI::GUI::CImageBox(this, (WINDOW_WIDTH - 644) / 2 + 30 + 40, (WINDOW_HEIGHT - 200) + 65, 22, 22);
 	m_pVolumeLow->SetImageId(GUI_THEME_SALLY_AUDIO_LOW);
@@ -58,29 +53,53 @@ CVolumePopUp::CVolumePopUp(SallyAPI::GUI::CGUIBaseObject* parent)
 	m_pVolumeHigh = new SallyAPI::GUI::CImageBox(this, (WINDOW_WIDTH - 644) / 2 + 552 + 40, (WINDOW_HEIGHT - 200) + 65, 22, 22);
 	m_pVolumeHigh->SetImageId(GUI_THEME_SALLY_AUDIO_HIGH);
 	this->AddChild(m_pVolumeHigh);
+
+	m_pHideTimer = new SallyAPI::GUI::CTimer(15, this, 0, HIDE_TIMER);
 }
 
 CVolumePopUp::~CVolumePopUp()
 {
+	m_pHideTimer->WaitForStop();
+	SafeDelete(m_pHideTimer);
 }
 
 void CVolumePopUp::UpdateView()
 {
 	m_pVolumeControl->UpdateView();
+	m_pHideTimer->Reset();
+	m_pHideTimer->Start();
 }
 
 void CVolumePopUp::SendMessageToParent(SallyAPI::GUI::CGUIBaseObject* reporter, int reporterId, int messageId, SallyAPI::GUI::SendMessage::CParameterBase* messageParameter)
 {
 	switch (messageId)
 	{
+	case HIDE_TIMER:
+		m_pHideTimer->Stop();
+		m_pParent->SendMessageToParent(this, 0, MS_SALLY_HIDE_VOLUME);
+		return;
 	case GUI_BUTTON_CLICKED:
 		switch (reporterId)
 		{
 		case GUI_THEME_SALLY_CLOSE:
+			m_pHideTimer->Stop();
 			m_pParent->SendMessageToParent(this, 0, MS_SALLY_HIDE_VOLUME);
 			return;
 		}
 		break;
 	}
 	SallyAPI::GUI::CPopUpWindow::SendMessageToParent(reporter, reporterId, messageId, messageParameter);
+}
+
+bool CVolumePopUp::ProcessMouseDown(int x, int y)
+{
+	m_pHideTimer->Stop();
+	return SallyAPI::GUI::CPopUpWindow::ProcessMouseDown(x, y);
+}
+
+bool CVolumePopUp::ProcessMouseUp(int x, int y)
+{
+	m_pHideTimer->Reset();
+	m_pHideTimer->Start();
+	return SallyAPI::GUI::CPopUpWindow::ProcessMouseUp(x, y);
 }
