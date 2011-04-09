a541c2e0ca0674ab9fb0b05bfe8d662b70b0bf6f
derknob <derknob@9afc4399-3f22-6faa-62dd-eea3a5e6d859>
2011-04-09 17:56:56 +0000

    Release 0.7.0.1

---
+++ branches/branch-0-7-0-1/sally/sallyAPI/ListViewExt.h
@@ -0,0 +1,123 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \file	sallyAPI\ListViewExt.h
+///
+/// \brief	Declares the list view ext class. 
+///
+/// \author	Christian Knobloch
+/// \date	23.11.2010
+///
+/// This file is part of the Sally Project
+/// 
+/// Copyright(c) 2008-2010 Sally Project
+/// http://www.sally-project.org/
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+/// 
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU Lesser General Public License for more details.
+/// 
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include "Define.h"
+#include "Form.h"
+#include <vector>
+#include "ListViewButton.h"
+#include "ListViewItem.h"
+#include "Box2DObject.h"
+#include "Scrollbar.h"
+#include "ParameterListItem.h"
+
+#define LISTVIEW_ITEM_HEIGHT		CONTROL_HEIGHT
+
+namespace SallyAPI
+{
+	namespace GUI
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// \class	CListViewExt
+		///
+		/// \brief	List view ext.
+		///
+		/// \author	Christian Knobloch
+		/// \date	26.04.2010
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		class DLL_API_SALLY CListViewExt :
+			public SallyAPI::GUI::CForm
+		{
+		protected:
+			std::vector<SallyAPI::GUI::CListViewItem*>		m_vItems;
+			SallyAPI::GUI::CScrollbar*						m_pScrollbar;
+			std::map<int, std::map<int, SallyAPI::GUI::CListViewButton*>>	m_mButton;
+
+			int				m_iStartItem;
+			int				m_iCols;
+			int				m_iRows;
+			int				m_iActive;
+			b2World*		m_pb2World;
+			CBox2DObject*	m_pb2Object;
+			b2Body*			m_pb2LeftBody;
+			b2Body*			m_pb2RightBody;
+			int				m_iOldPositionX;
+			int				m_iOldPositionY;
+			std::map<int, int> m_mWidthList;
+			
+			void	ResetListView();
+			int		GetMaxItemStart();
+
+			bool	ResetBox2Object();
+
+			void	OnCommandScrollbarMoved(SallyAPI::GUI::CGUIBaseObject* reporter, SallyAPI::GUI::SendMessage::CParameterBase* messageParameter);
+			void	OnCommandDoubleclicked(int reporterId);
+			void	OnCommandMouseMove(SallyAPI::GUI::SendMessage::CParameterBase* messageParameter);
+
+			virtual void AddChild(SallyAPI::GUI::CControl* control);
+		public:
+			CListViewExt(SallyAPI::GUI::CGUIBaseObject* parent, int x, int y, int width, int height, 
+				int rows, std::map<int, int> widthList, int controlId = 0);
+			virtual ~CListViewExt();
+
+			virtual void	Resize(int width, int height);
+
+			int			GetMaxDisplayedElements();
+			int			GetListSize();
+
+			void		AddItem(SallyAPI::GUI::CListViewItem listItem);
+			bool		RemoveItem(int index);
+
+			void		UpdateView();
+			void		Clear();
+			std::vector<SallyAPI::GUI::CListViewItem*>*	GetListItems();
+			SallyAPI::GUI::CListViewItem*				GetItem(int index);
+
+			void		SetActive(int index);
+			int			GetActive();
+
+			int			GetStartItem();
+			void		SetStartItem(int startItem);
+
+			void		SetPictureList(std::vector<int>& pictureList);
+			void		RemovePictureList();
+			void		SetActionImageID(int icon);
+
+			void		ShowScrollbarIfNotScrollable(bool value);
+			bool		IsScrollbarVisibleIfNotScrollbable();
+
+			virtual void SetAlphaBlending(int alphaBlending);
+			virtual bool CheckProcessMouseUp(int x, int y);
+			virtual bool CheckProcessMouseDown(int x, int y);
+			virtual void Enable(bool enable);
+			virtual void Timer(float timeDelta);
+			virtual void SendMessageToChilds(SallyAPI::GUI::CGUIBaseObject* reporter, int reporterId, int messageId, SallyAPI::GUI::SendMessage::CParameterBase* messageParameter = NULL);
+			virtual void SendMessageToParent(SallyAPI::GUI::CGUIBaseObject* reporter, int reporterId, int messageId, SallyAPI::GUI::SendMessage::CParameterBase* messageParameter = NULL);
+		};
+	}
+}
---
+++ branches/branch-0-7-0-1/sallyPlugins/plugins/MediaPlayer/PlaylistManager.h
@@ -0,0 +1,79 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \file	MediaPlayer\PlaylistManager.h
+///
+/// \brief	Declares the playlist manager class. 
+///
+/// \author	Christian Knobloch
+/// \date	29.11.2010
+///
+/// This file is part of the Sally Project
+/// 
+/// Copyright(c) 2008-2010 Sally Project
+/// http://www.sally-project.org/
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+/// 
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU General Public License for more details.
+/// 
+/// You should have received a copy of the GNU General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include <sallyAPI\sallyAPI.h>
+#include "Playlist.h"
+
+class CPlaylistManager :
+	public SallyAPI::GUI::CForm
+{
+private:
+	CPlaylist*		m_pPlaylist;
+
+	// for the thread
+	SallyAPI::GUI::CThreadStarter*	m_pTSAddToList;
+	std::string						m_strListToLoad;
+	bool							m_bSetAutoPlaylistName;
+
+
+	SallyAPI::GUI::CButtonBar*			m_pMenu;
+	SallyAPI::GUI::CButtonBarButton*	m_pMenuPlaylist;
+	SallyAPI::GUI::CButtonBarButton*	m_pMenuAutoPlaylist;
+
+	SallyAPI::GUI::CButton*				m_pButtonClear;
+	SallyAPI::GUI::CEdit*				m_pEditSearch;
+
+	SallyAPI::GUI::CButtonBar*			m_pMenuSort;
+	SallyAPI::GUI::CButtonBarButton*	m_pMenuSortName;
+	SallyAPI::GUI::CButtonBarButton*	m_pMenuSortDate;
+
+	SallyAPI::GUI::CListViewExt*		m_pListViewToDeleteFrom;
+	int									m_iItemToDelete;
+	
+	SallyAPI::GUI::CListViewExt*		m_pFileBrowserCurrent;
+	SallyAPI::GUI::CListViewExt*		m_pFileBrowserMyPlaylists;
+	SallyAPI::GUI::CListViewExt*		m_pFileBrowserAutoPlaylists;
+
+	void	OnCommandShowAutoPlaylists();
+	void	OnCommandShowMyPlaylists();
+	void	OnCommandSortByName();
+	void	OnCommandSortByDate();
+
+	void	OnCommandDeleteFile();
+	void	OpenFolder(SallyAPI::GUI::CListViewExt* listView, std::string folder);
+	void	AddListThread();
+	void	OnCommandListViewClicked(SallyAPI::GUI::CGUIBaseObject* reporter, SallyAPI::GUI::SendMessage::CParameterBase* messageParameter);
+	void	OnCommandAddList(SallyAPI::GUI::SendMessage::CParameterBase* messageParameter, SallyAPI::GUI::CListViewExt* listView, bool setAutoPlaylistName);
+	void	OnCommandDeletePlaylist(SallyAPI::GUI::SendMessage::CParameterBase* messageParameter, SallyAPI::GUI::CListViewExt* listView);
+public:
+	CPlaylistManager(SallyAPI::GUI::CGUIBaseObject* parent, int graphicId, CPlaylist* playlist);
+	virtual ~CPlaylistManager();
+
+	void			ReloadFileList();
+	virtual void	SendMessageToParent(SallyAPI::GUI::CGUIBaseObject *reporter, int reporterId, int messageId, SallyAPI::GUI::SendMessage::CParameterBase* messageParameter = NULL);
+};
