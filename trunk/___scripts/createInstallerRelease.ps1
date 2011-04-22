484d9ba40fc5019940377f8319326bf2a6f06503
derknob <derknob@9afc4399-3f22-6faa-62dd-eea3a5e6d859>
2011-04-22 13:58:46 +0000

    France folder needs to removed recurse

--- trunk/___scripts/createInstallerRelease.ps1
+++ trunk/___scripts/createInstallerRelease.ps1
@@ -51,7 +51,7 @@ remove-item $startDir"sally\__setup\install\languages\sally-project.language.de\
 remove-item $startDir"sally\__setup\install\languages\sally-project.language.de\de.der-knob.sally.app.cdvd.lang" -ErrorAction SilentlyContinue
 remove-item $startDir"sally\__setup\install\languages\sally-project.language.de\org.scummvm.sally.app.lang" -ErrorAction SilentlyContinue
 
-remove-item $startDir"sally\__setup\install\languages\sally-project.language.fr" -ErrorAction SilentlyContinue
+remove-item $startDir"sally\__setup\install\languages\sally-project.language.fr" -recurse -ErrorAction SilentlyContinue
 
 # remove pdbs
 echo "-- deleting pdb files plugins"
