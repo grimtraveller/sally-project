20dff11c2e376320a39a7ad0489caa1120bedead
DerRealKnob <DerRealKnob@gmail.com>
2011-12-23 18:15:37 +0000

    [SA-00498] update dev Wiki

--- ___scripts/createApiInstaller.ps1
+++ ___scripts/createApiInstaller.ps1
@@ -13,6 +13,9 @@ new-item $startDir"sally\__apiInstall\lib\lib\" -type directory -verbose
 # copy the files
 copy-item $startDir"sally\sally\sallyAPI\*.h" $startDir"sally\__apiInstall\include\include\sallyAPI\" -verbose
 
+copy-item $startDir"sally\__apiExt\include\*" $startDir"sally\__apiInstall\include\include\" -recurse -verbose
+copy-item $startDir"sally\__apiExt\lib\*" $startDir"sally\__apiInstall\lib\lib\" -recurse -verbose
+
 copy-item $startDir"sally\sally\Release\sallyAPI.lib" $startDir"sally\__apiInstall\lib\lib\" -verbose
 copy-item $startDir"sally\sally\Release\sallyAPI.pdb" $startDir"sally\__apiInstall\lib\lib\" -verbose
 
