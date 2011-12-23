d02cfd601ee2bd8b2fe65e3b4079b3767789076c
derrealknob@gmail.com <derrealknob@gmail.com@9afc4399-3f22-6faa-62dd-eea3a5e6d859>
2011-12-23 08:42:33 +0000

    corrected the drawing

--- trunk/sally/sallyAPI/Processbar.cpp
+++ trunk/sally/sallyAPI/Processbar.cpp
@@ -108,14 +108,10 @@ void CProcessbar::RenderControl()
 
 			int i = (int)(position - imageWidthLeft);
 
-			if (position <= m_iWidth - imageWidthRight)
-			{
-				DrawImage(GUI_THEME_PROCESSBAR_2, x + imageWidthLeft, y, i, m_iHeight);
-			}
-			else
-			{
-				DrawImage(GUI_THEME_PROCESSBAR_2, x + imageWidthLeft, y, m_iWidth - (imageWidthLeft + imageWidthRight), m_iHeight);
-			}
+			if (i > m_iWidth - (imageWidthLeft + imageWidthRight))
+				i = m_iWidth - (imageWidthLeft + imageWidthRight);
+
+			DrawImage(GUI_THEME_PROCESSBAR_2, x + imageWidthLeft, y, i, m_iHeight);
 
 			DrawImage(GUI_THEME_PROCESSBAR_2RIGHT, x + imageWidthLeft + i, y);
 		}
